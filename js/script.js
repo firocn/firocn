9f93a5aacfb84f5e745e8abb15298439a73ec39b
Kuro <kuroguo@outlook.com>
2022-02-23 16:54:09 +0800

    Update menu

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ Array.prototype.forEach.call(document.querySelectorAll('img'), img => {
 })
 
 if (document.referrer.includes(window.siteURL)) {
-  const returnLink = document.querySelector('#return_link')
-  returnLink.href = 'javascript:history.back()'
-  returnLink.innerHTML = '返回'
+  const footerHome = document.querySelector('#footer_home')
+  footerHome.style.display = 'none'
 }
