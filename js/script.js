042994d49bc0cba927774644fbe6e4db219c8212
Kuro <kuroguo@outlook.com>
2022-03-01 18:03:17 +0800

    Auto show header and topbtn

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@ if (document.referrer.includes(window.siteURL)) {
   const footerHome = document.querySelector('#footer_home')
   if (footerHome) footerHome.style.display = 'none'
 }
+
+;(function () {
+  const header = document.querySelector('body > header')
+  const scrollToTop = document.querySelector('#scroll_to_top')
+  header.style.position = 'sticky'
+  window.addEventListener('scroll', e => {
+    if (window.pageYOffset < window.lastPageY) {
+      document.body.classList.add('show-header')
+      document.body.classList.remove('hide-header')
+      if (window.pageYOffset > 0) {
+        document.body.classList.add('show-topbtn')
+      } else {
+        document.body.classList.remove('show-topbtn')
+      }
+    } else {
+      document.body.classList.remove('show-header')
+      document.body.classList.add('hide-header')
+      document.body.classList.remove('show-topbtn')
+    }
+    window.lastPageY = window.pageYOffset
+  })
+})()
