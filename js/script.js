09a52ab8e5526f4270512ba73e3a44bd26d8e84c
Kuro <kuroguo@outlook.com>
2022-03-09 06:53:40 +0800

    Update header transition

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
+let bodyHeight = document.body.offsetHeight
+let windowHeight = window.innerHeight
+
+window.addEventListener('resize', () => { windowHeight = window.innerHeight })
+
 Array.prototype.forEach.call(document.querySelectorAll('img'), img => {
   if (img.complete) return img.classList.add('complete')
   img.addEventListener('load', e => {
     e.target.classList.add('complete')
+    bodyHeight = document.body.offsetHeight
   })
 })
 
@@ -13,12 +19,16 @@ if (document.referrer.includes(window.siteURL)) {
 ;(function () {
   const header = document.querySelector('body > header')
   const scrollToTop = document.querySelector('#scroll_to_top')
-  header.style.position = 'sticky'
-  const deadzone = 10
+  const deadzone = 10, headerHeight = header.offsetHeight
   let lastPageY
-  window.addEventListener('scroll', e => {
+  window.addEventListener('scroll', () => {
     const scrollY = window.pageYOffset
-    if (scrollY < lastPageY - deadzone || scrollY < deadzone) {
+    if (scrollY < headerHeight) {
+      header.classList.add('visible')
+    } else {
+      header.classList.remove('visible')
+    }
+    if (scrollY < lastPageY - deadzone || scrollY < deadzone || scrollY + windowHeight >= bodyHeight - 100) {
       document.body.classList.add('show-header')
       document.body.classList.remove('hide-header')
       if (scrollY > 0) {
@@ -34,3 +44,5 @@ if (document.referrer.includes(window.siteURL)) {
     lastPageY = scrollY
   })
 })()
+
+document.body.classList.add('initialized')
