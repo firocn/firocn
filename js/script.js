b4134ae7f536a83c3a73050d7e6a99590824e0e0
Kuro <kuroguo@outlook.com>
2022-03-09 01:11:28 +0800

    Update deadzone of show-header

--- js/script.js
+++ js/script.js
@@ -14,20 +14,23 @@ if (document.referrer.includes(window.siteURL)) {
   const header = document.querySelector('body > header')
   const scrollToTop = document.querySelector('#scroll_to_top')
   header.style.position = 'sticky'
+  const deadzone = 10
+  let lastPageY
   window.addEventListener('scroll', e => {
-    if (window.pageYOffset < window.lastPageY) {
+    const scrollY = window.pageYOffset
+    if (scrollY < lastPageY - deadzone || scrollY < deadzone) {
       document.body.classList.add('show-header')
       document.body.classList.remove('hide-header')
-      if (window.pageYOffset > 0) {
+      if (scrollY > 0) {
         document.body.classList.add('show-topbtn')
       } else {
         document.body.classList.remove('show-topbtn')
       }
-    } else if (window.pageYOffset > window.lastPageY + 10) {
+    } else if (scrollY > lastPageY + deadzone) {
       document.body.classList.remove('show-header')
       document.body.classList.add('hide-header')
       document.body.classList.remove('show-topbtn')
     }
-    window.lastPageY = window.pageYOffset
+    lastPageY = scrollY
   })
 })()
